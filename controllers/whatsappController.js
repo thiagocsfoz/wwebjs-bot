7cf2f079db143aa53302c9d56ce6391365f7daa9
Thiago Chaves Santos <demandasworkana@grupolance.com.br>
2024-06-07 23:07:30 -0300

    update number

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -1,5 +1,5 @@
 const { initializeClient, clients } = require('../services/whatsappService');
-const QRCode = require('qrcode')
+const QRCode = require('qrcode');
 
 exports.generateQr = async (req, res) => {
     console.log('generateQr');
@@ -14,11 +14,49 @@ exports.generateQr = async (req, res) => {
 
     client.once('qr', async (qr) => {
         console.log(`QR code for ${assistantId}`);
-        // qrcode.generate(qr, { small: true }, (qrcodeStr) => {
-        //     res.send(`${qrcodeStr}`);
-        // });
-        // r
         const qrCodeDataUrl = await QRCode.toDataURL(qr);
         return res.json({ qrCodeUrl: qrCodeDataUrl });
     });
+};
+
+exports.checkConnection = (req, res) => {
+    console.log('checkConnection');
+    const { assistantId } = req.body;
+
+    const client = clients[assistantId];
+
+    if (client) {
+        if (client.info && client.info.wid) {
+            console.log(`Client is connected with number: ${client.info.wid.user}`);
+            return res.json({ connected: true, phoneNumber: client.info.wid.user });
+        } else {
+            console.log(`Client is not connected for assistantId: ${assistantId}`);
+            return res.json({ connected: false, phoneNumber: null });
+        }
+    } else {
+        console.log(`No client found for assistantId: ${assistantId}`);
+        return res.json({ connected: false, phoneNumber: null });
+    }
+};
+
+exports.disconnectPhone = async (req, res) => {
+    console.log('disconnectPhone');
+    const { assistantId } = req.body;
+
+    const client = clients[assistantId];
+
+    if (client) {
+        try {
+            await client.logout();
+            delete clients[assistantId];
+            console.log(`Client disconnected and removed for assistantId: ${assistantId}`);
+            return res.json({ success: true, message: 'Client disconnected successfully' });
+        } catch (error) {
+            console.error(`Error disconnecting client for assistantId: ${assistantId}`, error);
+            return res.status(500).json({ success: false, message: 'Error disconnecting client', error });
+        }
+    } else {
+        console.log(`No client found for assistantId: ${assistantId}`);
+        return res.json({ success: false, message: 'No client found' });
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { generateQr } = require('../controllers/whatsappController');
+const { generateQr, checkConnection, disconnectPhone } = require('../controllers/whatsappController');
 
 const router = express.Router();
 
 router.post('/generate_qr', generateQr);
+router.post('/check_connection', checkConnection);
+router.post('/disconnect_phone', disconnectPhone);
 
 module.exports = router;
