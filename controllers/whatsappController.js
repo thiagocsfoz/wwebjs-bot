7a6e3704e44fbc77bb79824fb8f6474aebd54c7e
Thiago Chaves Santos <demandasworkana@grupolance.com.br>
2024-06-08 00:42:58 -0300

    update logs

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -39,7 +39,7 @@ exports.generateQr = async (req, res) => {
     } catch (error) {
         console.error('Error initializing clients:', error);
     } finally {
-        await client.close();
+        await mongoClient.close();
     }
 };
 
